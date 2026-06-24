dead1f17745e8fe174de49ce665552baa78f8daf
invakid404 <invakid404@riseup.net>
2026-06-24 18:51:18 +0300

    chore(baml-rest): update to `0.0.48`

--- Formula/baml-rest.rb
+++ Formula/baml-rest.rb
@@ -5,7 +5,7 @@ class BamlRest < Formula
   desc "CLI tool for BAML REST server"
   homepage "https://github.com/invakid404/baml-rest"
   url "https://github.com/invakid404/baml-rest.git",
-      tag: "0.0.47"
+      tag: "0.0.48"
   license "MIT"
 
   depends_on "go" => :build
